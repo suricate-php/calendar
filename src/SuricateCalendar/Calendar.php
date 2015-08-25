2c101c56104cb51c977277260b4f5822da57e6e4
Mathieu LESNIAK <maverick@eskuel.net>
2015-08-25 23:32:19 +0200

    Month mode
    Params are now in an array, instead of defining lots of setter.
    Days (and hours in day mode) are now managed with a array container

--- src/SuricateCalendar/Calendar.php
+++ src/SuricateCalendar/Calendar.php
@@ -4,36 +4,219 @@ namespace SuricateCalendar;
 
 class Calendar
 {
+    const DAY_MODE          = 'day';
+    const WEEK_MODE         = 'week';
+    const MONTH_MODE        = 'month';
+
+    protected $container                = [];
+
     protected $displayDays  = false;
     protected $calendarId   = 1;
-    protected $locale       = 'fr_FR';
-    protected $date;
+    protected $currentDate;
+    protected $currentTimestamp;
+    protected $day;
+    protected $month;
+    protected $year;
+    protected $params       = [];
     
-    public function __construct()
+    public function __construct($date = null, $params = [])
     {
-        $this->date = date('Ymd');
+        $defaultParams = [
+            'id'            => 1,
+            'locale'        => 'fr_FR.UTF8',
+            'displayMode'   => self::MONTH_MODE,
+
+            // Month params
+            'showDays'      => true,
+            'showMonth'     => true,
+
+
+            // week params
+
+            // day params
+
+            // merged params
+            'showFullDayName' => true,
+            
+            'weekStartOn'   => 'sunday',
+            ];
+
+        if ($date === null) {
+            $date = date('Ymd');
+        }
+        $this->currentDate      = $date;
+        $this->currentTimestamp = strtotime($date);
+        $this->year             = strftime('%Y', $this->currentTimestamp);
+        $this->month            = strftime('%m', $this->currentTimestamp);
+        $this->day              = strftime('%d', $this->currentTimestamp);
+
+        $this->params = array_merge($defaultParams, $params);
+
+        $this->initContainer();
     }
 
-    public function setLocale($locale)
+    public function __get($var)
     {
-        $this->locale = $locale;
-
-        return $this;
+        if (array_key_exists($var, $this->params)) {
+            return $this->params[$var];
+        } else {
+            return null;
+        }
     }
 
-    public function setDate($date)
+    private function initContainer()
     {
-        return $this;
+        if ($this->displayMode == self::DAY_MODE) {
+            $this->initDayContainer();
+        } elseif ($this->displayMode == self::WEEK_MODE) {
+            $this->initWeekContainer();
+        } else {
+            $this->initMonthContainer();
+        }
+    }
+
+    private function initDayContainer()
+    {
+
+    }
+
+    private function initWeekContainer()
+    {
+
+    }
+
+    private function initMonthContainer()
+    {
+        $firstDayOfMonth = mktime(0,0,0, $this->month, 1, $this->year);
+        $firstDayOffset  = date('w', $firstDayOfMonth);
+
+        $nbDaysInMonth   = date('t', $this->currentTimestamp);
+        $nbDays          = $nbDaysInMonth;
+
+        if ($this->weekStartOn == 'monday') {
+            $firstDayOffset = ($firstDayOffset == 0) ? 7 : $firstDayOffset;
+        }
+        $startOffset    = -1 * ($firstDayOffset - 1);
+        $endOffset      = 7 - (abs($nbDaysInMonth - $startOffset) % 7);
+        
+        for ($i = $startOffset; $i < $nbDays + $endOffset; $i++) {
+            $ts = strtotime($i . ' days', $firstDayOfMonth);
+            $dayIdentifier = date('Ymd', $ts);
+            $item = new CalendarItem(date('d', $ts));
+            if ($i < 0) {
+                $item->past = true;
+            } elseif ($i >= $nbDaysInMonth) {
+                $item->future = true;
+            }
+
+            $this->container[$dayIdentifier] = $item;
+        }
     }
 
     public function render()
     {
-        $oldLocale = setlocale(LC_TIME);
-        setlocale(LC_TIME, $this->locale);
+        $output = '';
 
+        $oldLocale = setlocale(LC_TIME, 0);
+        
+        if (!setlocale(LC_TIME, $this->locale)) {
+            throw new \InvalidArgumentException("Missing locale on system : " . $this->locale);
+        }
+
+        $output = '<table border="1" class="calendar" id="calendar-' . $this->id . '">';
+
+        switch ($this->displayMode) {
+            case self::DAY_MODE:
+                $output .= $this->renderDay();
+                break;
+            case self::WEEK_MODE:
+                $output .= $this->renderWeek();
+                break;
+            default:
+                $output .= $this->renderMonth();
+                break;
+        }
+        
+        $output .= '</table>';
 
         // Restore previous locale
         setlocale(LC_TIME, $oldLocale);
 
+        return $output;
     }
+
+    protected function renderDay()
+    {
+        $output = '';
+
+        return $output;
+    }
+
+    protected function renderWeek()
+    {
+        $output = '';
+
+        return $output;
+    }
+
+    protected function renderMonth()
+    {
+        $strDayName     = $this->showFullDayName ? '%A' : '%a';
+
+        $output = '';
+
+        if ($this->showMonth) {
+            $output .= '<tr>';
+            $output .= '    <td colspan="7">';
+            $output .=          strftime('%B', $this->currentTimestamp);
+            $output .= '    </td>';
+            $output .= '</tr>';
+        }
+        if ($this->showDays) {
+            $baseTs = strtotime('next ' . $this->weekStartOn);
+            $output .= '<tr>';
+            for ($i = 0; $i < 7; $i++) {
+                $output .= '<td>';
+                $output .=     strftime($strDayName, $baseTs + 3600 * 24 * $i);
+                $output .= '</td>';
+            }
+            $output .= '</tr>';
+
+        }
+        $i = 0;
+        foreach ($this->container as $offset => $day) {
+            if ($i % 7 == 0) {
+                $output .= '<tr>';
+            }
+            $classes = ['day'];
+
+            if ($day->future) {
+                $classes[] = 'future';
+            } elseif ($day->past) {
+                $classes[] = 'past';
+            }
+            if (count($day->items)) {
+                $classes[] = 'has-item';
+            }
+
+            if ($offset == $this->currentDate) {
+                $classes[] = 'active';
+            }
+            $cellClass = ' class="' . implode(' ', $classes) . '"';
+           
+            $output .= '<td' . $cellClass . '>';
+            $output .= '    ' . $day->number;
+            $output .= '</td>';
+            
+            if ($i % 7 == 6) {
+                $output .= '</tr>';
+            }
+            $i++;
+        }
+
+        return $output;
+    }
+
+
+
 }
--- src/SuricateCalendar/CalendarItem.php
+++ src/SuricateCalendar/CalendarItem.php
@@ -4,6 +4,9 @@ namespace SuricateCalendar;
 class CalendarItem
 {
     public $number;
+    public $past    = false;
+    public $future  = false;
+    public $items   = [];
 
 
     public function __construct($number = null)
